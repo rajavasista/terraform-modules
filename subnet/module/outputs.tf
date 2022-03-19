8850ec5fe6d49da4c5c72ba6f6a282202a0d210c
rajavasista <vasistatatapudi@gmail.com>
2022-03-19 12:05:10 +0000

    updated

--- subnet/module/outputs.tf
+++ subnet/module/outputs.tf
@@ -1,11 +1,11 @@
 output "my_public_subnet_id" {
-  value = aws_subnet.my_public_subnet.id
+  value = aws_subnet.my_pub_subnet.id
 }
 
 output "my_public_subnet_arn" {
-  value = aws_subnet.my_public_subnet.arn
+  value = aws_subnet.my_pub_subnet.arn
 }
 
 output "my_public_subnet_cidr_block" {
-  value = aws_subnet.my_public_subnet.cidr_block
+  value = aws_subnet.my_pub_subnet.cidr_block
 }
