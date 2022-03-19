7161ce8745771b687f6d42bfb7c6c31bd3648f5b
rajavasista <vasistatatapudi@gmail.com>
2022-03-19 12:00:20 +0000

    added

--- vpc/module/main.tf
+++ vpc/module/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = join("_", var.project, "VPC")
+    Name = join("-", var.project, "VPC")
     Project = var.project
   }
 }
